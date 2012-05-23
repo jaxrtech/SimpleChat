44ddf134ff51d1132d21eef88c53341dc254a8ac
Joshua Bowden <josbow@gmail.com>
2012-05-23 16:26:21 -0400

    Add provisional support for a command system
    Start to work on the command system that would be later used with all the commands for everything. This is provisional and is not currently used yet since it is not completed.
    
    Library:
    + Started command system

---
+++ ChatLib/Protocol/ProtocolCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChatLib.Protocol
+{
+    public abstract class ProtocolCommand
+    {
+        protected string Name { get; set; }
+
+        protected ProtocolParameter[] Parameters { get; set; }
+
+        protected string Serialize()
+        {
+            string command = "";
+
+            command += Name;
+
+            foreach (var param in Parameters)
+            {
+                // TODO: get the front param first and then the rest
+            }
+
+            return "";
+        }
+    }
+}
