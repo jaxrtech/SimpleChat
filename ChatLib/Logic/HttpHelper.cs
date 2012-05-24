22f84f79bef6212fa57f5dfa4e4a73f0571ac1af
Joshua Bowden <josbow@gmail.com>
2012-05-24 19:45:21 -0400

    Move HTTP GET code to another class and add ablity to get the port number from a text file
    This was a quick fix that moved HTTP get command to its own class so that it can be easilly used. Also added some future support for getting the port number off of a text file from the web if needed.
    
    Client:
    - Moved HTTP GET command to HtmlHelper
    - Added future support for getting the port number from a text file

---
+++ ChatLib/Logic/HttpHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace ChatLib
+{
+    public class HttpHelper
+    {
+        public static string Get(string url)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Timeout = 5000;
+                request.Method = "GET";
+
+                WebResponse responce = request.GetResponse();
+                StreamReader reader = new StreamReader(responce.GetResponseStream(), Encoding.UTF8);
+                string content = reader.ReadToEnd();
+                reader.Close();
+                responce.Close();
+
+                return content;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
