af2d61dc86e10f14fa6f3bbb6e49bf1b129ae024
Joshua Bowden <josbow@gmail.com>
2012-06-02 00:57:35 -0400

    Add project for testing command protocol serialization
    Since the command protocol's code needs to be tested, another project just to test it was created. This will help make sure that the serialization is actually working right.
    
    Library:
    + Protocol test project

---
+++ ProtocolTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ChatLib.Protocol;
+
+namespace ProtocolTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SimpleChat Protocol Serialization Tester\n");
+
+            // Serialize
+            MessageCommand command = new MessageCommand();
+            command.Sender = "jaxrtech";
+            command.Content = "Hello World!";
+
+            Console.WriteLine("== Orginal");
+            WriteCommandInfo(command);
+
+            string message = CommandSerializer.Serialize(command);
+            Console.WriteLine("== Serialized\n" + message);
+
+            // Deserialize
+            /* TODO: Make it be able to go into its orginal form
+             *       This can probably be accomplished by writing a converter between
+             *       Command -> MessageCommand or something like that or even ICommand or something
+             *       So that they can be able to be converted back
+             *       The use of generics may also be required too
+             */ 
+            Command deCommand = CommandSerializer.Deserialize(message);
+            Console.WriteLine("== Desrialized");
+            WriteCommandInfo(deCommand);
+
+            Console.WriteLine("== Done");
+            Console.ReadLine();
+        }
+
+        static void WriteCommandInfo(ICommand command)
+        {
+            Console.WriteLine(
+                "Command Object " + "<" + command.GetType().ToString() + ">" + "\n" +
+                "Type=" + command.Type + "\n" +
+                "Name=" + command.Name + "\n" +
+                "HasSender=" + command.HasSender + "\n" +
+                "Sender=" + command.Sender + "\n" +
+                "HasContent=" + command.HasContent + "\n" +
+                "Content=" + command.Content + "\n"
+            );
+        }
+    }
+}
